b03e3070f4732583fb1cd11d01c6991659983c5b
Vitor Pinto <vi.deap@gmail.com>
2024-11-29 14:15:13 -0300

    added linter

--- main.ts
+++ main.ts
@@ -1,28 +1,8 @@
 import * as THREE from 'three';
 
-// Configura o cenário, a câmera e o renderizador
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// Configuration Values
+let render_sizes: [number, number] = [window.innerWidth, window.innerHeight];
+
+// Creating the renderer
 const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Cria um cubo
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-// Posiciona a câmera
-camera.position.z = 5;
-
-// Animação
-const animate = () => {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
-
-animate();
+renderer.setSize(render_sizes[0], render_sizes[1]);
