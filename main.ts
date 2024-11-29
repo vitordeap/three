c995115877f3431ee23948acce3c23c3bd274ba8
Vitor Pinto <vi.deap@gmail.com>
2024-11-29 14:33:20 -0300

    added scene

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 import * as THREE from 'three';
 
-// Configuration Values
+// Configuration Values (later they must be loaded at start of a session)
 let render_sizes: [number, number] = [window.innerWidth, window.innerHeight];
 
 // Creating the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(render_sizes[0], render_sizes[1]);
+document.body.appendChild(renderer.domElement);
+
+// Creating a scene
+const scene = new THREE.Scene();
+
+// Creating a camera
+const camera = new THREE.PerspectiveCamera(
+    90,
+    render_sizes[0] / render_sizes[1],
+);
+
+// Render!
+renderer.render(scene, camera);
